151bab581f6144da6d8ba45204e32830a4221612
ups7write <anatoli.mihov@gmail.com>
2020-02-25 22:22:31 +0200

    Адаптация под 576px. Работа с accordion.

--- src/js/script.js
+++ src/js/script.js
@@ -11,4 +11,14 @@ $(document).ready(function () {
 		loop: true,
 		grabCursor: true,
 	});
+
+	// script for accordion footer list
+	$(".accordion > .accordion__item.is-active").children(".accordion-panel").slideDown();
+
+	$(".accordion > .accordion__item").click(function () {
+		// Cancel the siblings
+		$(this).siblings(".accordion__item").removeClass("is-active").children(".accordion-panel").slideUp();
+		// Toggle the item
+		$(this).toggleClass("is-active").children(".accordion-panel").slideToggle("ease-out");
+	});
 });
