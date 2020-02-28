68c837acb5647df5f35be7c24506726fda9019dd
ups7write <anatoli.mihov@gmail.com>
2020-02-29 00:54:01 +0200

    created an accordion block in the footer block max-width 767.98px. Connected cdn sources. Update structure.

--- src/js/script.js
+++ src/js/script.js
@@ -12,13 +12,9 @@ $(document).ready(function () {
 		grabCursor: true,
 	});
 
-	// script for accordion footer list
-	$(".accordion > .accordion__item.is-active").children(".accordion-panel").slideDown();
 
-	$(".accordion > .accordion__item").click(function () {
-		// Cancel the siblings
-		$(this).siblings(".accordion__item").removeClass("is-active").children(".accordion-panel").slideUp();
-		// Toggle the item
-		$(this).toggleClass("is-active").children(".accordion-panel").slideToggle("ease-out");
+	$(function () {
+		$('#menu').metisMenu();
 	});
+
 });
